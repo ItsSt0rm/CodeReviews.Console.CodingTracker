601f2ebcb426dace44d10a2181c93b0202d2c31b
Storm <esteban.palaciov@hotmail.com>
2023-09-21 22:32:53 -0500

    Config file & Database Manager

---
+++ CodingTracker.ItsSt0rm/DatabaseManager.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTracker.ItsSt0rm
+{
+    internal class DatabaseManager
+    {
+        private string _connectionString;
+
+        public DatabaseManager(string connectionString) 
+        { 
+            _connectionString = connectionString;
+        }
+
+        public void InitializeDatabase()
+        {
+            using (var connection = new SqliteConnection(_connectionString))
+            {
+                connection.Open();
+                var tableCmd = connection.CreateCommand();
+
+                tableCmd.CommandText =
+                    @"CREATE TABLE IF NOT EXISTS coding_tracker (
+                    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    Date TEXT,
+                    StartTime TEXT,
+                    EndTime TEXT,
+                    Duration INTEGER
+                    )";
+
+                tableCmd.ExecuteNonQuery();
+
+                connection.Close();
+            }
+        }
+    }
+}
---
+++ CodingTracker.ItsSt0rm/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Configuration;
+using System.Collections.Specialized;
+using Microsoft.Data.Sqlite;
+
+namespace CodingTracker.ItsSt0rm
+{
+    internal class Program
+    {
+        static string connectionString = ConfigurationManager.AppSettings.Get("connectionString");
+
+        static void Main(string[] args)
+        {
+            DatabaseManager dbManager = new DatabaseManager(connectionString);
+            dbManager.InitializeDatabase();
+        }
+    }
+}
